bf6cb0e03f1d27f5cdd83ff58a09256c5776c6cf
linjie <xialjsummer@163.com>
2023-11-29 15:22:25 +0000

    my first commit

---
+++ linjie.php
@@ -0,0 +1,10 @@
+<?php
+// Loop through numbers from 1 to 10
+for ($i = 1; $i <= 10; $i++) {
+    // Check if the number is odd
+    if ($i % 2 != 0) {
+        // Print the odd number
+        echo $i . "\n";
+    }
+}
+?>
